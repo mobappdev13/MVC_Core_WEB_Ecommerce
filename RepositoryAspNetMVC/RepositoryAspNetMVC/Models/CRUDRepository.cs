3d8aaec885fedcaf40ddd1f5d5b02291b110ad7b
Carmen Vargas <mobappdev1@gmail.com>
2018-03-13 23:47:35 +0100

    add ICRUDRepository Interface, CRUDRepository

---
+++ RepositoryAspNetMVC/RepositoryAspNetMVC/Models/CRUDRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using RepositoryAspNetMVC.Models;
+
+namespace RepositoryAspNetMVC.Models
+{
+    public class CRUDRepository<T> : ICRUDRepository<T> where T : class
+    {
+
+        private Ecommerce19Entities db;
+
+        private DbSet<T> table;
+
+        //ctor
+        public CRUDRepository()
+        {
+            db = new Ecommerce19Entities();
+            table = db.Set<T>();
+        }
+
+
+        //interface method's
+        public long count()
+        {
+            return table.Count(); 
+        }
+
+        //interface method's
+        public void create(T obj)
+        {
+            table.Add(obj);
+            //todo try catch
+            db.SaveChanges();
+        }
+
+        //interface method's
+        public void delete(object id)
+        {
+            T tRow = table.Find(id);
+            table.Remove(tRow);
+            //todo try catch
+            db.SaveChanges();
+        }
+
+        //interface method's
+        public T find(object id)
+        {
+            // todo try catch
+                return table.Find(id);
+           
+            //catch (Exception)
+            //{
+            //    return null;
+            //}
+        }
+
+        //interface method's
+        public IEnumerable<T> findAll()
+        {
+            return table.ToList();
+        }
+
+        //interface method's
+        public void update(T obj)
+        {
+            table.Attach(obj);
+            db.Entry(obj).State = EntityState.Modified;
+            //todo try catch
+            db.SaveChanges();
+        }
+    }
+}
---
+++ RepositoryAspNetMVC/RepositoryAspNetMVC/Models/ICRUDRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryAspNetMVC.Models
+{
+    public interface ICRUDRepository<T> where T: class
+    {
+        IEnumerable<T> findAll();
+        T find(object id);
+        void create(T obj);
+        void delete(object id);
+        void update(T obj);
+        long count();
+    }
+}

ccce337c21ef1063ee53ca9aad4a2930d4bdf801
Carmen Vargas <mobappdev1@gmail.com>
2018-03-13 17:32:04 +0100

    test repositories for Categories and Products Icons for login

---
+++ RepositoryAspNetMVC/RepositoryAspNetMVC/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RepositoryAspNetMVC.Models;
+
+namespace RepositoryAspNetMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICRUDRepository<Category> categoryCRUDRepository;
+
+        public CategoryController()
+        {
+            categoryCRUDRepository = new CRUDRepository<Category>();
+        }
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            ViewBag.categories = categoryCRUDRepository.findAll()
+            .OrderBy(c => c.CategoryId)
+            .ThenBy(c => c.Description);
+
+            return View();
+        }
+    }
+}
---
+++ RepositoryAspNetMVC/RepositoryAspNetMVC/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RepositoryAspNetMVC.Models;
+
+namespace RepositoryAspNetMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ICRUDRepository<Product> productCRUDRepository;
+        // GET: Product
+
+        public ProductController()
+        {
+            productCRUDRepository = new CRUDRepository<Product>();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.products = productCRUDRepository.findAll()
+            .OrderBy(p => p.ProductId)
+            .ThenBy(p => p.CategoryId)
+            .ThenBy(p => p.Description);
+
+            return View();
+        }
+    }
+}
